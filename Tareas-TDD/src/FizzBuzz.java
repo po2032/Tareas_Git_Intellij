ab901e4ba2f8ac22df8aaf59d9367efcce35fd7f
dam1t <po2032@gmail.com>
2023-03-07 19:52:42 +0100

    Creada la clase FizzBuzz y corregido el metodo y los Tests

--- Tareas-TDD/src/FizzBuzz.java
+++ Tareas-TDD/src/FizzBuzz.java
@@ -3,8 +3,11 @@ import javax.management.StringValueExp;
 public class FizzBuzz {
 
     public String pedirNum(int num){
-        num = StringValueOf(num);
-
-        return ;
+        String num2 = "";
+        if (num%3 == 0){
+            return "Fizz";
+        } else{
+            return num2 + num;
+        }
     }
 }
--- Tareas-TDD/src/FizzBuzzTest.java
+++ Tareas-TDD/src/FizzBuzzTest.java
@@ -7,14 +7,14 @@ class FizzBuzzTest {
     @Test
     void devuelve1enCadena(){
         FizzBuzz numCalcular = new FizzBuzz();
-        numCalcular.pedirNum(1);
-        assertEquals(1,numCalcular);
+        String numACalcular = numCalcular.pedirNum(1);
+        assertEquals("1",numACalcular);
     }
     @Test
     void devuelve2enCadena(){
         FizzBuzz numCalcular = new FizzBuzz();
-        numCalcular.pedirNum(2);
-        assertEquals(2,numCalcular);
+        String numACalcular = numCalcular.pedirNum(2);
+        assertEquals("2",numACalcular);
     }
 
 }
